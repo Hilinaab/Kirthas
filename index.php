ed61c2cf36dd46eba42979c0cbb9877cfe244ea5
Hilina Ayalew Berhanu <hilinawab@gmail.com>
2023-11-30 20:38:28 +0300

    Src

---
+++ index.php
@@ -0,0 +1,177 @@
+<!DOCTYPE html>
+
+<html lang="">
+<head>
+<title>KIRTHAS</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row1">
+  <header id="header" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div id="logo" class="fl_left">
+      <h1><a href="index.php">KIRTHAS</a></h1>
+    </div>
+    <nav id="mainav" class="fl_right">
+      <ul class="clear">
+        <li class="active"><a href="contactus.php">Home</a></li>
+        
+        <li><a class="drop" href="">SIGNIN</a>
+          <ul>
+            <li><a href="pages/login.php">LogIn</a></li>
+            <li><a href="pages/register.php">SIGNUP</a></li>
+          
+      
+           
+          </ul>
+
+        </li>
+        <li class =""><div id="google_translate_element"></div></a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </header>
+</div>
+</div>
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/1.png');">
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+     
+      <h2 class="heading"></h2>
+    </br>
+  </br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+
+</br>
+
+     
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+
+
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+
+
+    <iframe width="1440" height="900" src="https://www.youtube.com/embed/D3cs7DGTNQs" frameborder="0" allow="accelerometer;  controls autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+  </div>
+   
+    </div>
+    </div>
+<!-- ################################################################################################ -->
+
+<div class="wrapper row4">
+    <div class="wrapper bgded overlay" style="background-image:url('images/demo/backgrounds/3.jpg'); no-repeat;">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+        <br>
+
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+  
+  </main>
+</div>
+</div>
+</div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+   
+    <ul class="faico clear">
+    <li><a class="faicon-facebook" href="https://www.facebook.com/kirthas.kirthas.7"><i class="fa fa-facebook"></i></a></li>
+    <li><a class="faicon-twitter" href="https://twitter.com/kirthas2"><i class="fa fa-twitter"></i></a></li>
+  
+    <li><a class="faicon-google-plus" href="https://www.instagram.com/kirthas12/"><i class="fa fa-instagram"></i></a></li>
+   
+  </ul>
+    <nav>
+      <ul class="nospace">
+        <li><a href="#"><i class="fa fa-lg fa-home"></i></a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">Privacy</a></li>
+        <li><a href="#">Disclaimer</a></li>
+        <li><a href="#">Cookies</a></li>
+      </ul>
+    </nav>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">KIRTHAS</a></p>
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script>
+
+<script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+<script src="layout/scripts/jquery.flexslider-min.js"></script>
+</body>
+</html>
